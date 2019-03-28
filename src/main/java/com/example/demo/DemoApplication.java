73af245e63d06695e905ebf54156a9412e43d0df
xiongpc <xpc_v587@163.com>
2019-03-28 23:17:49 +0800

    init project!

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xpc
+ * @date 2019/2/28 0:53
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+    }
+
+}
